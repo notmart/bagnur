4f14eb43e67fa31aef0efce71072ad495fe97431
Marco Martin <notmart@gmail.com>
2016-03-28 15:34:45 +0200

    translate to english

--- bagnur.ino
+++ bagnur.ino
@@ -4,20 +4,20 @@
 Soil Moisture Chart
  0 : in air @ 24c
  120 : skin
- 0 - 250 : BAGNATO
+ 0 - 250 : WET
  300 - 500 : OK
- 500 - 1021 : ASCIUTTO
+ 500 - 1021 : DRY
  
 ------------------------------
 */
  
 
-int ledblu = 3;
-//usa il 5 perchè analogico
-int ledverde = 5;
-int ledrosso = 6;
-//il 2 non è analogico
-int rubinetto = 2;
+int blueLed = 3;
+//use ping 5 because analog
+int greenLed = 5;
+int redLed = 6;
+//2 not analog
+int tapPin = 2;
 
 int numCampioniAsciutto = 0;
 
@@ -30,10 +30,10 @@ double intensity = 1;
 void setup()
 {
   Serial.begin(9800);
-  pinMode(ledblu,OUTPUT);
-  pinMode(ledverde,OUTPUT);
-  pinMode(ledrosso,OUTPUT);
-  pinMode(rubinetto,OUTPUT);
+  pinMode(blueLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  pinMode(redLed,OUTPUT);
+  pinMode(tapPin,OUTPUT);
 }
 
 void fade(int red, int green, int blue, double newIntensity)
@@ -53,21 +53,21 @@ void fade(int red, int green, int blue, double newIntensity)
        if (fabs(intensity - newIntensity) > (double)(0.02)) {
            intensity += intensity > newIntensity ? -0.01 : 0.01;
        }
-       analogWrite(ledrosso, (int)((double)currentRed * intensity));
-       analogWrite(ledverde, (int)((double)currentGreen * intensity));
-       analogWrite(ledblu, (int)((double)currentBlue * intensity));
+       analogWrite(redLed, (int)((double)currentRed * intensity));
+       analogWrite(greenLed, (int)((double)currentGreen * intensity));
+       analogWrite(blueLed, (int)((double)currentBlue * intensity));
        delay(20);
    }
 }
 
-//attorno a 250: verde
-//attorno a 1000: rosso
+//around a 250: green
+//around a 1000: red
 //valori intermedi: via di mezzo
 void fadeFromSensorValue(int sensorValue, double newIntensity)
 {
     int normalized = ((double)(sensorValue)/(double)1023) * 254;
-    Serial.print("Valore sensore normalizzato: ");
-    Serial.println(normalized);
+    //Serial.print("Sensor value normalized 0-255: ");
+    //Serial.println(normalized);
     fade(normalized, 255 - normalized, 0, newIntensity);
 }
 
@@ -82,43 +82,45 @@ void loop()
   Serial.print("Manual dial value: ");
   Serial.println(dialValue);
 
-  Serial.print("Letture asciutto:");
+  Serial.print("Number of dry samplings:");
   Serial.println(numCampioniAsciutto);
 
-  int moistureAdjustment = ((double)dialValue/(double)1024 - (double)0.5) * 100;
-  Serial.print("Correzione moisture: ");
+  int moistureAdjustment = ((double)dialValue/(double)1024 - (double)0.5) * 200;
+  Serial.print("Moisture correction: ");
   Serial.println(moistureAdjustment);
 
-  if (sensorValue < 250 - moistureAdjustment) { 
-      Serial.println("bagnato");
+  Serial.print(sensorValue + moistureAdjustment);
+ 
+  if (sensorValue < 150 - moistureAdjustment) { 
+      Serial.println(": wet");
       numCampioniAsciutto = 0;
-  } else if (sensorValue >= 250 - moistureAdjustment && sensorValue < 500 - moistureAdjustment) { 
-      Serial.println("OK");
+  } else if (sensorValue >= 150 - moistureAdjustment && sensorValue < 400 - moistureAdjustment) { 
+      Serial.println(": OK");
   } else { 
-      Serial.println("asciutto");
+      Serial.println(": dry");
       numCampioniAsciutto++;
   }
   
   fadeFromSensorValue(sensorValue, 1);
 
   if (numCampioniAsciutto >= 10) {
-      digitalWrite(rubinetto, HIGH);
-      //chiudi un contatto per 2 secondi poi riaprilo: usabile sia per
-      //un rubinetto a relais che per un cazzillo fatto con il servo
+      digitalWrite(tapPin, HIGH);
+      //Open the solenoid-controlled watering tap
       fade(0, 0, 255, 1);
       //da 1 a 30 secondi
-      Serial.print("Apro il rubinetto per millisecondi:");
+      Serial.print("Water tap open for milliseconds:");
       Serial.println(1000 + (dialValue * 20));
       delay(1000 + (dialValue * 30));
       numCampioniAsciutto = 0;
       fadeFromSensorValue(analogRead(5), 1);
-      digitalWrite(rubinetto, LOW);
+      digitalWrite(tapPin, LOW);
   }
 
   delay(1000);
-  Serial.println("Fade sensore");
+  Serial.println("Led Fade");
   fadeFromSensorValue(sensorValue, 0.01);
-
+  Serial.println("=========== Wait for next sampling ==============");
+ 
   delay(10000);
 }
 
